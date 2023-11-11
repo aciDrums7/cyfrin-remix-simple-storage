b85781ea1222db7eaa81a187d9d5fd499d84fb39
acidrums7 <edoardo.carradori.96@gmail.com>
2023-11-11 12:43:21 +0100

    Fixed wrong override of SimpleStorage contract

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,33 +1,43 @@
-// I'm a comment!
 // SPDX-License-Identifier: MIT
-
 pragma solidity 0.8.19;
 
-// pragma solidity ^0.8.0;
-// pragma solidity >=0.8.0 <0.9.0;
-
 contract SimpleStorage {
-    uint256 myFavoriteNumber;
+    // favoriteNumber gets initialized to 0 if no value is provided
+    // Default visibility is 'internal'
+    uint256 myFavoriteNumber; // 0
 
-    struct Person {
-        uint256 favoriteNumber;
+    // uint256[] favoriteNumbersList;
+
+    struct Person{
         string name;
+        uint256 favoriteNumber;
     }
-    // uint256[] public anArray;
-    Person[] public listOfPeople;
+    
+    // dynamic array
+    Person[] public peopleList;
+
+    // static array
+    // Person[3] public peopleList;
 
     mapping(string => uint256) public nameToFavoriteNumber;
 
-    function store(uint256 _favoriteNumber) public virtual {
+    function store(uint256 _favoriteNumber) public {
         myFavoriteNumber = _favoriteNumber;
+        // this call will cost gas, even if retrieve() is a view function
+        // getFavoriteNumber();
     }
-
-    function retrieve() public view returns (uint256) {
+    // view -> read state from blockchain
+    // pure -> doesn't change state of blockchain
+    function getFavoriteNumber() public view returns(uint256) {
         return myFavoriteNumber;
     }
-
+    
+    // by default, func params are memory vars
+    // calldata -> temp param, CANNOT BE MODIFIED
+    // memory -> temp param, CAN BE MODIFIED
+    // storage -> permanent var, CAN BE MOFIDIED
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
-        listOfPeople.push(Person(_favoriteNumber, _name));
+        peopleList.push( Person(_name, _favoriteNumber) );
         nameToFavoriteNumber[_name] = _favoriteNumber;
     }
 }
